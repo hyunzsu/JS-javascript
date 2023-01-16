96e7916de8043dff6c7e16e6132cfc73a3f2345e
hyunzsu <kkm5169@naver.com>
2023-01-17 00:55:37 +0900

    [추가] 레코드 리스트 보이기

--- client/lib/dom/controlElement.js
+++ client/lib/dom/controlElement.js
@@ -2,9 +2,20 @@
 
 // 2. disable 활성 유틸 함수 만들기
 export function disableElement(node) {
-  node.disabled = true;
+  node.disabled = true; // 비활성화, 클릭 허용 안됨
 }
 
 export function enableElement(node) {
-  node.disabled = false;
+  node.disabled = false; // 활성화, 클릭 허용 됨
+}
+
+
+// 4. visible 활성 유틸 함수 만들기
+// 기록 버튼을 누르면 리스트가 등장하게 설정
+export function visibleElement(node) {
+  node.hidden = false; // 클래서에서 hidden 없애면 리스트가 보임
+}
+
+export function invisibleElement(node) {
+  node.hidden = true; // html요소에 hidden이 true니까 리스트가 안보임
 }
--- client/main.js
+++ client/main.js
@@ -1,22 +1,27 @@
-// [주사위 굴리기]
+// [주사위 굴리기] [1 ~ 8]
 // 1. dice 애니메이션 불러오기
 // 2. bindEvent 유틸함수 만들기
 // 3. handleRollingDice 함수 만들고 토글로 애니메이션 제어하기
 // 4. 변수 보호를 위한 클로저 + IIFE 사용하기
 
-// [ 레코드 리스트 보이기 ] [9 ~ ]
+// [ 레코드 리스트 보이기 ] [9 ~ 18]
 // 1. handleRecord 함수를 만들기
 // 2. disable 활성 유틸 함수 만들기
 // 3. handleReset 함수를 만듭니다.
 // 4. visible 활성 유틸 함수 만들기
 // 5. toggleState 유틸 함수 만들기 
 
-import { diceAnimation, disableElement, enableElement, getNode } from "./lib/index.js";
+import { diceAnimation, disableElement, enableElement, getNode, invisibleElement, visibleElement } from "./lib/index.js";
 
 
 const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
 const recordButton = getNode('.buttonGroup > button:nth-child(2)');
+const resetButton = getNode('.buttonGroup > button:nth-child(3)');
 
+const recordListWrapper = getNode('.recordListWrapper') // (14) .recordListWrapper getNode 한 것 변수에 담아줌
+
+
+// (2) rollingDiceButton 이벤트핸들러 연결
 const handlerRollingDice = () => { 
 
   let stopAnimation;
@@ -26,22 +31,29 @@ const handlerRollingDice = () => {
     if (!isRolling) {
       stopAnimation = setInterval(diceAnimation,100); // 시간간격 두고 반복실행 (연결시켜 놓은 애니메이션으로))
       disableElement(recordButton); // (12) 비활성화. 주사위버튼 실행중이면 기록버튼 누를 수 없음
+      disableElement(resetButton); // (17) 비활성화. 주사위버튼 실행중이면 초기화버튼 누를 수 없음
     } else {
       clearInterval(stopAnimation); // 이전에 설정된 시간 제한 반복 작업을 취소시킴
       enableElement(recordButton); // (12) 활성화. 주사위버튼 멈추면 기록버튼 누를 수 있음
+      enableElement(resetButton); // (17) 활성화. 주사위버튼 멈추면 기록버튼 누를 수 있음
     }
   
     isRolling = !isRolling; // true, false 토글 됨
-
   }
 
   
 }
 
-const handleRecord = () => {
+// (13) recordButton 이벤트핸들러 연결
+const handlerRecord = () => {
+  visibleElement(recordListWrapper) // (15) 기록버튼 누르면 레코드리스트가 보임
+}
 
+// (16) resetButton 이벤트핸들러 연결
+const handlerReset = () => {
+  invisibleElement(recordListWrapper) // (18) 초기화버튼 누르면 레코드리스트 사라짐
 }
 
 rollingDiceButton.addEventListener('click', handlerRollingDice()); // IIFE 사용하지 않고 클로저하면 뒤에 () 붙여야 함
-recordButton.addEventListener('click', handleRecord);
-
+recordButton.addEventListener('click', handlerRecord); // (13) 기록버튼 눌렀을 때 이벤트핸들러 실행
+resetButton.addEventListener('click', handlerReset); // (16) 초기화버튼 눌렀을 때 이벤트핸들러 실행
