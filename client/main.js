a669e34d18d4a8ba4a001e28d79957a8205efaa2
hyunzsu <kkm5169@naver.com>
2023-01-21 03:09:26 +0900

    [추가] delay 콜백

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import { xhrData, insertLast } from './lib/index.js';
 
 
 // insertLast -> 화면에 뿌려줌 (user 1 정보)
-xhrData.get(
+/* xhrData.get(
   'https://jsonplaceholder.typicode.com/users/1',
   (res) => {
     insertLast('body', JSON.stringify(res)); // onSuccess
@@ -10,4 +10,4 @@ xhrData.get(
   (err) => {
     insertLast('body', '데이터 로딩에 실패했습니다.'); // onFail
   }
-);
+); */
