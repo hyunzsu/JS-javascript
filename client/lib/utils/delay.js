f639a78ad58813d11cc2488e747b6ee3db948db7
hyunzsu <kkm5169@naver.com>
2023-01-21 14:40:23 +0900

    [추가] callback 지옥 탈출하기

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -12,7 +12,8 @@ function delay(callback, timeout = 1000) {
   setTimeout(callback, timeout);
 }
 
-delay(()=>{
+// 콜백지옥
+/* delay(()=>{
   first.style.top = '-100px'
   delay(()=>{
     first.style.transform = 'rotate(360deg)'
@@ -20,6 +21,39 @@ delay(()=>{
       first.style.top = '0px'
     })
   })
+}) */
+
+// promise 이용 -> 콜백함수의 가독성을 위해
+function delayP(timeout = 1000) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('성공!');
+      // reject('실패!');
+    }, timeout);
+  });
+}
+
+delayP()
+.then(() => {
+  first.style.top = '-100px';
+  return delayP();
 })
+.then(() => {
+  first.style.transform = 'rotate(360deg)';
+  return delayP();
+})
+.then(() => {
+  first.style.top = '0px';
+});
+
+console.log(delayP()); // Promise, [PromiseState]: 'fulfilled', [PromiseResult]: '성공!'
 
 
+// 위에 결과를 가져옴. 성공한걸! 실패는 catch 구문
+/* promise.then((res)=>{
+	console.log(res) // 성공입니다.
+}) 
+
+promise.cathch((err)=>{
+	console.log(err) // 실패입니다.
+}) */
