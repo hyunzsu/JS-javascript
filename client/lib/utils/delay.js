a3413be747944f9478826212b8c0023bf2be1ea6
hyunzsu <kkm5169@naver.com>
2023-01-21 16:06:23 +0900

    [추가] 객체 합성 mixin

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -55,9 +55,29 @@ delayP()
 console.log(delayP()); // Promise, [PromiseState]: 'fulfilled', [PromiseResult]: '성공!'
 */
 
-// return 쓰는거 귀찮!
-// default parameter, argument는 추가하고싶은거 따로 넘겨줌
-function delayP(shouldReject = false, timeout = 1000, data = '성공했습니다.', errorMessage = '알 수 없는 오류가 발생했습니다.') {
+
+// 매개변수 자리마다 맞추기 힘드니까 구조분해할당 말고 다른 형태 시도~!
+// 내가 원하는 값만 넣어주고 다른것들은 기본값으로 하고싶어 -> 기본값은 기본으로 깔고, 우리가 변경하고 싶은 값만 새롭게 반영
+// argument 일일히 다 넘기기 귀찮, 필요한것만 넘길거~ 다른것들은 기본값
+
+// 기본값 설정
+const defaultOptions = {
+  shouldReject: false,
+  timeout: 1000,
+  data: '성공했습니다.',
+  errorMessage: '알 수 없는 오류가 발생했습니다.'
+}
+
+function delayP(options = {}) {
+
+  // defaultOptions
+  let config = {...defaultOptions} // 얕복, 새롭게 담김, 왜 하냐? 참조에 의한 복사하면 기존 값까지 바뀌어버리는 사태가 발생...!
+  
+  // 객체 합성 mixin
+  config = {...config, ...options}; // 기존 config객체 + 우리가 넣을 options 추가하기(options값이 config값과 동일하면 options값으로 덮어주기)
+
+  const {shouldReject, data, errorMessage, timeout} = config; // 새로운 config로 구조 분해 할당
+
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (!shouldReject) {
@@ -69,14 +89,8 @@ function delayP(shouldReject = false, timeout = 1000, data = '성공했습니다
   })
 }
 
-delayP(false, 1000, '진짜 성공', '오류가 발생했다!!').then((res)=>{
-  console.log(res); // 진짜 성공
-}) 
-
-
-// resolve, reject
-/*
-delayP()
-.then((res) => console.log(res)); // resolve
-.catch((err) => console.log(err)); // reject
-*/
+delayP({
+  data: '안녕',
+}).then((res) => {
+  console.log(res); // 안녕
+})
