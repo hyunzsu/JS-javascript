c591d32401a9b06b249db2b5cb4d95f9479be4e6
hyunzsu <kkm5169@naver.com>
2023-01-21 15:42:52 +0900

    [추가] delayP parameter 설정하기

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -1,6 +1,7 @@
 import { getNode } from '../dom/getNode.js';
 
 const first = getNode('.first');
+const second = getNode('.second');
 
 // JS는 위에서 아래로 읽는데 같이 움직임, 콜백이 필요한 순간!
 /* first.style.top = '-100px';
@@ -14,16 +15,19 @@ function delay(callback, timeout = 1000) {
 
 // 콜백지옥
 /* delay(()=>{
-  first.style.top = '-100px'
+  first.style.top = '-100px';
   delay(()=>{
-    first.style.transform = 'rotate(360deg)'
+    second.style.left = '100px';
     delay(()=>{
-      first.style.top = '0px'
+      first.style.top = '0px';
+      second.style.left = '0px';
     })
+    first.style.transform = 'rotate(360deg)';
   })
 }) */
 
 // promise 이용 -> 콜백함수의 가독성을 위해
+/*
 function delayP(timeout = 1000) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -40,20 +44,39 @@ delayP()
 })
 .then(() => {
   first.style.transform = 'rotate(360deg)';
+  second.style.left = '100px';
   return delayP();
 })
 .then(() => {
   first.style.top = '0px';
+  second.style.left = '0px'
 });
 
 console.log(delayP()); // Promise, [PromiseState]: 'fulfilled', [PromiseResult]: '성공!'
+*/
 
+// return 쓰는거 귀찮!
+// default parameter, argument는 추가하고싶은거 따로 넘겨줌
+function delayP(shouldReject = false, timeout = 1000, data = '성공했습니다.', errorMessage = '알 수 없는 오류가 발생했습니다.') {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!shouldReject) {
+        resolve(data)    
+      } else {
+        reject(errorMessage)
+      }
+    }, timeout);
+  })
+}
 
-// 위에 결과를 가져옴. 성공한걸! 실패는 catch 구문
-/* promise.then((res)=>{
-	console.log(res) // 성공입니다.
+delayP(false, 1000, '진짜 성공', '오류가 발생했다!!').then((res)=>{
+  console.log(res); // 진짜 성공
 }) 
 
-promise.cathch((err)=>{
-	console.log(err) // 실패입니다.
-}) */
+
+// resolve, reject
+/*
+delayP()
+.then((res) => console.log(res)); // resolve
+.catch((err) => console.log(err)); // reject
+*/
