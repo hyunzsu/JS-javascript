634a0c082642fc90601322c581d82035c80997ef
hyunzsu <kkm5169@naver.com>
2023-01-22 00:56:11 +0900

    [추가] tiger 객체 합성

--- client/lib/utils/hyunzsu.js
+++ client/lib/utils/hyunzsu.js
@@ -8,24 +8,36 @@ const defaultOptions = {
   cache: 'no-cache',
   credential: 'same-origin',
   redirect:'follow',
-  referrerPlicy:'no-reffere',
+  referrerPolicy:'no-referrer',
   headers:{
     'Content-Type':'application/json; charset=UTF-8'
   }
 }
 
 
-const tiger = async () => {
-  let response = await fetch(
-    'https://jsonplaceholder.typicode.com/users/1',
-    {
-      method: 'GET',
-      headers:{
-        'Content-Type': 'application/json',
-      },
-    }
-  )
-  console.log(response);
+const tiger = async (options = {}) => {
+
+  // 객체합성과 동시에 url, ...restOptions(url을 제외한 나머지 객체?)만 구조분해할당
+  const {url, ...restOptions} = {
+    ...defaultOptions,
+    ...options,
+    headers: {...defaultOptions.headers, ...options.headers} // 깊은 복사
+  }
+  // console.log(restOptions);
+
+  let response = await fetch(url, restOptions) // 문법이 이럼
+
+  if (response.ok) { // fetch가 완료되었다면
+    response.data = await response.json() // data 추가, response 안에 있는 json 값을 가져옴 -> await은 값을 가져오는거니깐
+  }
+  
+  // console.log(response);
+
+  return response;
+
 }
 
-tiger()
+// tiger();
+
+
+
